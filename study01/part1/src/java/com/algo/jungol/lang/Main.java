b790ee195a16b8b43b299878e23d396ac427e91f
speed0dev <speed0dev@gmail.com>
2022-06-03 18:17:21 +0900

    test1

---
+++ study01/part1/src/java/com/algo/jungol/lang/Main.java
@@ -0,0 +1,12 @@
+package com.algo.jungol.lang;
+
+// 제출시 class Main 으로 제출 해야 함.
+public class Main {
+    public static void main(String[] args){
+        System.out.println("TTTTTTTTTT\n" +
+                "TTTTTTTTTT\n" +
+                "    TT\n" +
+                "    TT\n" +
+                "    TT");
+    }
+}
