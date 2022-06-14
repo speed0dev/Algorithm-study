a13d2cf85693cb1cfb439076de1fda7c84157126
speed0dev <speed0dev@gmail.com>
2022-06-14 11:22:15 +0900

    test

--- study01/part1/src/test/com/test/jk/base/str/StringTest.java
+++ study01/part1/src/test/com/test/jk/base/str/StringTest.java
@@ -2,6 +2,9 @@ package com.test.jk.base.str;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringTest {
 
     @Test
@@ -10,6 +13,39 @@ public class StringTest {
         System.out.println(r);
         String tmp = String.format("% 2d", 9);
         System.out.println(tmp);
+    }
+
+    @Test
+    void compNumberTest(){
+        System.out.println("===============================");
+        Pattern p = Pattern.compile("[\\d-\\d]");
+
+        String str = "[A100-200B]";
+        Matcher m = p.matcher(str);
+        if(m.find()){
+            System.out.println(" find!! ");
+            System.out.println( m.groupCount() + " " + m.group(0) + " " + m.toString() );
+        }else{
+            System.out.println(" not find!! ");
+        }
+    }
+
+    //정규식 확인
+    @Test
+    void compTest(){
+        System.out.println("===============================");
+
+        Pattern p = Pattern.compile("^\\[.*]$"); //<--처리함
+
+        String str = "[100-200]";
+        Matcher m = p.matcher(str);
+        if(m.find()){
+            System.out.println(" find !!!");
+            System.out.println( m.group() );
+        }else{
+            System.out.println(" not find !!!");
+        }
+
     }
 
 }
